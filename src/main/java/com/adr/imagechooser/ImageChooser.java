0e7751709884b5578cc899b7694891d3634624cd
Adrián Romero <adrianromero@users.noreply.github.com>
2014-09-30 08:44:13 +0200

    Adding license header to source code

--- src/main/java/com/adr/imagechooser/ImageChooser.java
+++ src/main/java/com/adr/imagechooser/ImageChooser.java
@@ -1,8 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//    AwesomeIcon is a library to select and display images control for JavaFX
+//    Copyright (C) 2014 Adrián Romero Corchado.
+//
+//    This file is part of ImageChooser
+//
+//     Licensed under the Apache License, Version 2.0 (the "License");
+//     you may not use this file except in compliance with the License.
+//     You may obtain a copy of the License at
+//     
+//         http://www.apache.org/licenses/LICENSE-2.0
+//     
+//     Unless required by applicable law or agreed to in writing, software
+//     distributed under the License is distributed on an "AS IS" BASIS,
+//     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//     See the License for the specific language governing permissions and
+//     limitations under the License
+
 package com.adr.imagechooser;
 
 import com.adr.awesomeicon.AwesomeIcon;
